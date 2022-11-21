4774d9817f73ddddf4f757c06025a957a60b26f5
LouArlo <lulu.arlo1306@gmail.com>
2022-11-20 18:49:11 -0600

    endpoints of users modules

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const controller = require('../controllers/users')
-const router = express.Router()
+const express = require("express");
+const controller = require("../controllers/users");
+const router = express.Router();
+//const UsersScheme = require("../models/users");
 
-const path = 'users'
+const path = "users";
 
-// Ruta /usuarios
+// endpoints users
 
-router.get(
-    `/${path}`,
-     controller.getData
-     )
+router.get(`/${path}`, controller.getData);
 
+router.post(`/${path}`, controller.insertData);
 
+router.get(`/${path}/:id`, controller.getDataId);
 
-module.exports = router
+router.put(`/${path}/:id`, controller.putDataId);
+/* router.get(`/${path}`, controller.getSingle);
+
+router.post(`/${path}`, controller.updateSingle);
+ */
+
+module.exports = router;
