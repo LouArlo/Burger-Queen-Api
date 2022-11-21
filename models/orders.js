3eec8c7e988ce57927ea67047283516020ee29d7
LouArlo <lulu.arlo1306@gmail.com>
2022-11-21 17:05:11 -0600

    schema of orders

---
+++ models/orders.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const OrdersScheme = new mongoose.Schema(
+    {
+        client: {
+            type: String
+        },
+        productOrder:{ 
+            type: Array ({
+               
+                name:{
+                    type: String
+                },
+                qty:{
+                    type: Number
+                }
+             })
+        }, 
+         status: {
+            type: String
+        }
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+);
+
+OrdersScheme.plugin(mongoosePaginate)
+
+module.exports = mongoose.model('orders', OrdersScheme)
