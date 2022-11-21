e1b71d8da03db67f581e53542348737333477c17
LouArlo <lulu.arlo1306@gmail.com>
2022-11-20 18:59:45 -0600

    schema of users

---
+++ models/users.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const UsersScheme = new mongoose.Schema(
+    {      
+        
+        email:{
+            type: String,
+            required: true          
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String
+        } 
+        
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+);
+
+UsersScheme.plugin(mongoosePaginate)
+
+module.exports = mongoose.model('users', UsersScheme)
