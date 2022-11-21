6162266c50b3ca7853506b4bf4401d32e8bab13a
LouArlo <lulu.arlo1306@gmail.com>
2022-11-20 18:47:24 -0600

    CRUD of users module

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,81 @@
-exports.getData = (req, res) => {
-    res.send({data: 'proveniente de ruta ./users'})
+const model = require("../models/users.js");
+const UsersScheme = require("../models/users");
+
+const options = {
+  page: 1,
+  limit: 10,
+};
+
+//GET GLOBAL DATA
+/* exports.getData = (req, res) => {
+  model.paginate({}, options, (err, docs) => {
+    res.send({
+      items: docs,
+    });
+  });
+}; */
+
+//CREATE DATA
+/* exports.insertData = (req, res) => {
+  const data = req.body;
+  model.create(data, (err, docs) => {
+    if (err) {
+      res.send({ error: "Error" }, 422);
+    } else {
+      res.send({ data: docs });
+    }
+  });
+}; */
+
+//GET SINGLE DATA
+/* exports.getSingle = (req, res) => {
+
 }
+
+//UPDATE SINGLE  DATA
+exports.updateSingle = (req, res) => {
+    
+} */
+// DELETE DATA
+
+//OTHER METHOD TO INSERT DATA
+ exports.insertData =  (req, res) => {
+    const user= UsersScheme(req.body);
+    user
+    .save()
+   .then((data) => res.json(data))
+   .catch((error) => res.json({ message: error}));
+}; 
+
+exports.getData =  (req, res) => {
+  UsersScheme
+  .find()
+ .then((data) => res.json(data))
+ .catch((error) => res.json({ message: error}));
+}; 
+
+exports.getDataId = (':id', (req, res) => {
+  const { id } = req.params;
+  UsersScheme
+  .findById(id)
+ .then((data) => res.json(data))
+ .catch((error) => res.json({ message: error}));
+}); 
+
+exports.putDataId = (':id', (req, res) => {
+  const { id } = req.params;
+  const { email, password, role } = req.body;
+  UsersScheme
+  .updateOne({_id:id}, { $set: {email, password, role}})
+ .then((data) => res.json(data))
+ .catch((error) => res.json({ message: error}));
+}); 
+// esto es para ver que los datos enviados se estan recibiendo, pero no ha funcionado
+
+/* router.post('/users', (req,res) => {
+   const user = UsersScheme(req.body);
+   user
+   .save()
+   .then((data) => res.json(data))
+   .catch((error) => res.json({ message: error}));
+}); */
