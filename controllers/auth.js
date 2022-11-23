221803ab40c6e2a89069d38ddc5e87df157c3d92
LouArlo <lulu.arlo1306@gmail.com>
2022-11-22 23:01:55 -0600

    create the token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,45 @@
-exports.getData = (req, res) => {
-    res.send({data: 'proviene de ruta ./auth'})
-}
+ //const { find } = require('../models/products');
+const UsersScheme = require('../models/users')
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRET;
+
+
+
+ exports.postValidate = (':email', (req, res) => {
+   
+    const { email } = req.params;
+        const user = {
+            email
+        }
+        UsersScheme
+        .find( { email })
+        .then((data) => {
+            res.json({ data })
+            })
+        .catch((error) => {
+            res.json({message:error})
+            });
+ });
+
+ exports.postCreateToken = (':email:password', (req, res) => {
+    const { email, password } = req.params;
+    const user = {
+          email, password
+          }
+    jwt.sign({user}, secretKey, (err, token) => {
+        res.json({ token })
+         }) 
+    })          
+
+    /* if (userFinded==true) {
+            jwt.sign({user}, secretKey, (err, token) => {
+           res.json({ token })
+           }) 
+    } else {
+        res.send("usuario no registrado")
+
+    } */
+    
+
+
+       
